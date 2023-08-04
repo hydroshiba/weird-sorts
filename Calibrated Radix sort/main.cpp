917d0386df31e79b2bb61da5fc96daec22052fd3
hydroshiba <36961737+hydroshiba@users.noreply.github.com>
2023-08-05 00:28:37 +0700

    Reimplemented for readability and simplicity sake

--- Calibrated Radix sort/main.cpp
+++ Calibrated Radix sort/main.cpp
@@ -1,94 +1,37 @@
-#include <bits/stdc++.h>
-#include <chrono>
 #include "radix.h"
+#include "timer.h"
 
-using namespace std;
+#include <cassert>
+#include <vector>
+#include <numeric>
+#include <iostream>
+#include <algorithm>
+#include <random>
 
-mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
-long long get_rand(long long r) {
-    return uniform_int_distribution<long long> (0, r-1)(rng);
-}
-long long get_rand(long long l, long long r) {
-    return uniform_int_distribution<long long> (l, r)(rng);
-}
+int main() {
+    int n = 10000000;
+    std::vector<int> vec(n);
 
-using NumT = int;
-void std_sort(std::vector<NumT>& arr) {
-    std::sort(arr.begin(), arr.end());
-}
+    std::iota(vec.begin(), vec.end(), 1);
+    random_shuffle(vec.begin(), vec.end());
+	std::vector<int> arr(vec);
 
-void counting_sort(std::vector<NumT>& arr) {
-    const int FULL_MASK = (1<<16) - 1;
-    auto get_low = [] (NumT x) { return x & FULL_MASK; };
-    auto get_high = [] (NumT x) { return x >> 16; };
+	std::cout << "Start" << std::endl;
 
-    std::vector<int> cnt_low(1<<16, 0);  // frequencies of low bits
-    std::vector<int> cnt_high(1<<16, 0); // frequencies of high bits
-    for (auto x : arr) {
-        cnt_low[get_low(x)]++;
-        cnt_high[get_high(x)]++;
-    }
-    std::partial_sum(cnt_low.begin(), cnt_low.end(), cnt_low.begin());
-    std::partial_sum(cnt_high.begin(), cnt_high.end(), cnt_high.begin());
+    Timer timer;
+    timer.start();
+    radix(vec.begin(), vec.end());
+    auto time = timer.get();
 
-    int n = arr.size();
-    std::vector<NumT> tmp(n);            // stores temporary sort result
+    // for(int i = 0; i < n; ++i) std::cout << vec[i] << ' ';
 
-    // 1st pass: sort all numbers according to low bits
-    // e.g. if we sort using last 2 bits (instead of 16 bits)
-    // [8, 7, 10, 2, 4]
-    //
-    // -> in binary:
-    // [1000, 0111, 1010, 0010, 0100]
-    //
-    // -> sort low bits:
-    // [1000, 0100, 1010, 0010, 0111]
-    //
-    // -> in base 10:
-    // [8, 4, 10, 2, 7]
-    for (int i = n-1; i >= 0; --i) {
-        tmp[--cnt_low[get_low(arr[i])]] = arr[i];
-    }
+    std::cout << "Radix time elapsed: " << time << "ms\n";
+    assert(std::is_sorted(vec.begin(), vec.end()));
 
-    // 2nd pass: sort high bits
-    for (int i = n-1; i >= 0; --i) {
-        arr[--cnt_high[get_high(tmp[i])]] = tmp[i];
-    }
-}
+	timer.start();
+	std::sort(arr.begin(), arr.end());
+	time = timer.get();
 
-void auto_base(std::vector<NumT>& arr){
-    radix(arr.begin(), arr.end());
-}
-
-template<typename F>
-void benchmark(F func, std::vector<NumT> &arr, string name) {
-
-    auto start = std::chrono::steady_clock::now().time_since_epoch().count();
-    func(arr);
-    auto finish = std::chrono::steady_clock::now().time_since_epoch().count();
-
-    assert(std::is_sorted(arr.begin(), arr.end()));
-    cout << name << "\t\t: " << (finish - start) / 1000000000.0 << "s" << endl;
-}
-
-int main(int _, char* argv[]) {
-    const int N = 500000000;
-    cout << fixed << setprecision(7);
-
-    std::vector<NumT> tmp(N);
-    for (int i = 0; i < N; ++i) tmp[i] = get_rand(1, INT_MAX);
-
-    // Disqualified for being too slow
-    // std::vector<NumT> arr1 = tmp;
-    // benchmark(std_sort, arr1, "std::sort");
-
-    int tests = 10;
-
-    while(tests--){
-        std::vector<NumT> arr2 = tmp;
-        benchmark(counting_sort, arr2, "count_rr");
-
-        std::vector<NumT> arr3 = tmp;
-        benchmark(auto_base, arr3, "auto base radix");
-    }
+	std::cout << "std::sort time elapsed: " << time << "ms\n";
+    return 0;
 }
--- Calibrated Radix sort/radix.h
+++ Calibrated Radix sort/radix.h
@@ -1,132 +1,80 @@
-#include "vector"
-#include "iterator"
-#include "climits"
-#include "cmath"
-#include "cstring"
-#include "cstdint"
-#include "algorithm"
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <iterator>
+#include <numeric>
 
-/// The core sorting algorithm
-template<typename iterator>
-void _radix(iterator begin, iterator end, int64_t base = -1){
-	using type = typename std::iterator_traits<__typeof(iterator)>::value_type;
+// Arbitrary base logarithm of an integer, return floored value
 
-	const type maxval = std::abs(*max_element(begin, end, [](const type &a, const type &b){
-		return (std::abs(a) < std::abs(b));
-	}));
-
-	if(maxval == 0) return;
-
-	// It'd be nuts if someone sorts over 2 billion items but better safe than sorry
-	const uint64_t size = (end - begin);
-
-	// If not specified, calculate optimal base
-	if(base < 0){
-		// (N + base) * floor(log(V) / log(base) + 1)
-		auto f = [&](double_t x) -> double_t{ 
-			return (size + x) * (uint64_t) (log(maxval) / log(x) + 1);
-		};
-
-		// Loop until the ith root of V is less than 3
-		base = 2;
-
-		for(uint64_t i = 1, lim = log(maxval) / log(3); i <= lim; ++i){
-			uint64_t cur = pow(maxval, 1.0 / i);
-
-			if(f(cur) < f(base)) base = cur;
-			if(f(cur + 1) < f(base)) base = cur + 1;
-			if(f(cur - 1) < f(base)) base = cur - 1;
-		}
-	}
-
-	const uint64_t loops = (log(maxval) / log(base)) + 1;
-	std::vector<std::vector<uint64_t>> bucket(loops, std::vector<uint64_t>(base));
-	double_t invpow[loops];
-
-	// For each power of base, we calculate its inverse so that we can do a
-	// multiplication instead of division
-	double_t div = 1.0;	
-	for(uint64_t i = 0; i < loops; ++i){
-		invpow[i] = 1.0 / div;
-		div *= base;
-	}
-
-	// Histograms building
-	for(uint64_t i = 0; i < loops; ++i){
-		for(iterator it = begin; it < end; ++it){
-			type digit = std::abs(*it) * invpow[i];
-			type quo = digit * invpow[1];
-			digit -= quo * base;
-
-			++bucket[i][digit];
-		}
-
-		std::partial_sum(bucket[i].begin(), bucket[i].end(), bucket[i].begin());
-	}
-
-	// Reordering
-	std::vector<type> helper(size);
-
-	for(uint64_t i = 0; i < loops; ++i){
-		if(bucket[i][base - 1] == 0) continue;
-
-		if(i & 1){
-			for(iterator it = helper.end() - 1; it >= helper.begin(); --it){
-				type digit = std::abs(*it) * invpow[i];
-				type quo = digit * invpow[1];
-				digit -= quo * base;
-
-				uint64_t pos = bucket[i][digit] - 1;
-				*(begin + pos) = *it;
-
-				--bucket[i][digit];
-			}
-		}
-		else{
-			for(iterator it = (end - 1); it >= begin; --it){
-				type digit = std::abs(*it) * invpow[i];
-				type quo = digit * invpow[1];
-				digit -= quo * base;
-
-				uint64_t pos = bucket[i][digit] - 1;
-				helper[pos] = *it;
-
-				--bucket[i][digit];
-			}
-		}
-	}
-
-	// One last pass
-	if(loops & 1){
-		for(uint64_t pos = 0; pos < size; ++pos) 
-			*(begin + pos) = helper[pos];
-	}
+template<typename Type>
+unsigned int log(Type N, Type base) {
+	return (N ? 1 + log(N / base, base) : 0);
 }
 
-/// Handles negative elements
+// The core sorting algorithm
+
 template<typename iterator>
-void radix(iterator begin, iterator end, int64_t base = -1){
-	// Swapping all negative values to the front
-	// Then we sort the negative and positive segments separately
-	iterator left = begin, right = end - 1, pivot;
+void radix(iterator begin, iterator end){
+	using Type = typename std::iterator_traits<__typeof(iterator)>::value_type;
+	size_t size = (end - begin);
 
-	while(true){
-		while(left < right && *left < 0) ++left;
-		while(right >= left && *right > 0) --right;
-		if(left >= right) break;
+	Type max = *std::max_element(begin, end);
+	if(max == 0) return;
 
-		std::swap(*left, *right);
-		++left;
-		--right;
+	// The complexity function (N + base) * floor(log(V) / log(base) + 1)
+
+	auto f = [&](Type x) { 
+		return (size + x) * Type(log(max, x) + 1);
+	};
+
+	// Calculate optimal base, loop until the ith root of V is less than 3
+	// The base should be smaller than max value, using the same integer Type for the base is sufficient
+	
+	Type base = 2, lim = log(max, 3);
+
+	for(Type i = 1; i <= lim; ++i) {
+		Type new_base = pow(max, 1.0 / i), current = f(base);
+
+		// Since the base value is integer, we need to check the min value of {new_base - 1, new_base, new_base + 1}
+
+		if(f(new_base) < current) base = new_base;
+		if(f(new_base + 1) < current) base = new_base + 1;
+		if(new_base > 2 && (new_base - 1) < current) base = new_base - 1;
 	}
 
-	for(pivot = right; pivot <= left; ++pivot)
-		if(pivot >= begin && *pivot > 0) break;
+	size_t *bucket = new size_t[base]();
+	Type *temp = new Type[size]();
 
-	// Sort
-	_radix(begin, pivot, base);
-	_radix(pivot, end, base);
+	Type loops = log(max, base) + 1;
+	Type power = 1;
 
-    // Reversing the negative segment
-    std::reverse(begin, pivot);
+	for(Type i = 0; i < loops; ++i) {
+
+		// Histograms building
+
+		memset(bucket, 0, sizeof(size_t) * base);
+
+		for(iterator it = begin; it != end; ++it) {
+			Type digit = (*it / power) % base;
+			++bucket[digit];
+		}
+
+		std::partial_sum(bucket, bucket + base, bucket);
+
+		// Reordering
+
+		for(iterator it = (end - 1); it >= begin; --it) {
+			Type digit = (*it / power) % base;
+			--bucket[digit];
+			temp[bucket[digit]] = *it;
+		}
+
+		for(iterator it = begin; it != end; ++it)
+			*it = temp[it - begin];
+
+		power *= base;
+	}
+
+	delete[] bucket;
+	delete[] temp;
 }
